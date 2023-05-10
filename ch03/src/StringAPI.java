2c698121029534983e4607ba04f75461bd3d2d6c
HTLYWindows <behumble@yeah.net>
2023-05-10 15:31:19 +0800

    "page 50"

--- ch03/src/StringAPI.java
+++ ch03/src/StringAPI.java
@@ -8,7 +8,17 @@ public class StringAPI {
         // 2. int codePointAt(int index); => 返回索引处字符的Unicode值
         System.out.println(s.codePointAt(2));
 
-        // 3. int offsetByCodePoints(int statIndex, int cpCount) => 返回从索引index处偏移codePointOffset个代码点的索引
+        /*
+            3. int offsetByCodePoints(int statIndex, int cpCount) =>
+               返回从索引 index 处偏移 codePointOffset 个代码点的索引
+        */
         System.out.println(s.offsetByCodePoints(3, 2));
+
+        /*
+            4. int compareTo(String anotherString)
+        */
+        System.out.println(s.compareTo("we"));
+
+
     }
 }
